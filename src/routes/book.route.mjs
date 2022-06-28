8168b6e3f0ea806d52369fb268b8919f12cb7a8a
soyo-kaze <07sameersaini@gmail.com>
2022-06-28 14:48:47 +0530

    search book endpoint addedd

--- src/routes/book.route.mjs
+++ src/routes/book.route.mjs
@@ -1,4 +1,4 @@
-import { request, Router } from "express";
+import { Router } from "express";
 import bookModel from "../models/book.model.js";
 
 let bookRouter = new Router();
@@ -26,6 +26,7 @@ bookRouter.route("/add-book").post(async (request, response) => {
  * @todo: Make search functionality according to what user provides like
  * - author
  * - title
+ * @todo: Implement get endpoint to get all data of books from books collection
  */
 
 bookRouter.route("/search-book").post(async (request, response) => {
@@ -34,6 +35,21 @@ bookRouter.route("/search-book").post(async (request, response) => {
     let bookData = await bookModel.find({ isbn });
     if (bookData.length) {
       response.status(200).send({ success: true, data: bookData });
+    } else {
+      response.status(200).send({ success: false, data: null });
+    }
+  } catch (e) {
+    response
+      .status(500)
+      .send({ success: false, message: `Error occurred ${e}` });
+  }
+});
+
+bookRouter.route("/get-books").get(async (request, response) => {
+  try {
+    let booksData = await bookModel.find({});
+    if (booksData.length) {
+      response.status(200).send({ success: true, data: booksData });
     } else {
       response.status(200).send({ success: false, data: null });
     }
